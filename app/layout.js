d5066f7c4de774189874a44512949680ac55056b
ashfullstack <akashramsonule@gmail.com>
2024-11-22 22:45:00 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 import localFont from "next/font/local";
 import "./globals.css";
 import Navbar from "@/components/Navbar"; 
-import Shorten from "./shorten/page";
+
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -26,7 +26,7 @@ export default function RootLayout({ children }) {
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-red-50`}
       > 
       <Navbar/>
-        <Shorten/>
+       
         {children}
       </body>
     </html>
